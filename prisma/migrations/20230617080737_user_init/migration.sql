c66b55691928e864cbca936a78b0a40859ca8a75
ismet <kadyrovismet1999@gmail.com>
2023-06-17 18:30:59 +0500

    fix: user model

---
+++ prisma/migrations/20230617080737_user_init/migration.sql
@@ -0,0 +1,191 @@
+-- CreateTable
+CREATE TABLE "Branch" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Branch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "photo" TEXT,
+    "fullName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "passportSeries" TEXT NOT NULL,
+    "branchId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "role" TEXT,
+    "superAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" SERIAL NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "phoneNumbers" TEXT[],
+    "passportSeries" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Request" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "clientId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "branchId" INTEGER NOT NULL,
+    "b2cPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "Request_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RequestTour" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "requestId" INTEGER NOT NULL,
+    "ticketId" TEXT NOT NULL,
+    "tourId" TEXT NOT NULL,
+    "tourDestination" TEXT NOT NULL,
+    "flightDate" TIMESTAMP(3) NOT NULL,
+    "b2bPrice" INTEGER NOT NULL,
+    "b2cPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "RequestTour_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RequestTicket" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "requestId" INTEGER NOT NULL,
+    "ticketId" TEXT NOT NULL,
+    "ticketDestination" TEXT NOT NULL,
+    "flightDate" TIMESTAMP(3) NOT NULL,
+    "b2bPrice" INTEGER NOT NULL,
+    "b2cPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "RequestTicket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RequestVisa" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "requestId" INTEGER NOT NULL,
+    "visaId" TEXT NOT NULL,
+    "b2cPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "RequestVisa_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "requestId" INTEGER NOT NULL,
+    "clientId" INTEGER NOT NULL,
+    "invoiceAmount" INTEGER NOT NULL,
+    "invoiceStatus" TEXT NOT NULL,
+    "branchId" INTEGER NOT NULL,
+    "userId" INTEGER,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "rawAmount" INTEGER NOT NULL,
+    "rawCurrency" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "invoiceId" INTEGER NOT NULL,
+    "paymentAmount" INTEGER NOT NULL,
+    "branchId" INTEGER NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Currency" (
+    "id" SERIAL NOT NULL,
+    "currency" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date" TIMESTAMP(3) NOT NULL,
+    "value" INTEGER NOT NULL,
+
+    CONSTRAINT "Currency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Branch_name_key" ON "Branch"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_passportSeries_key" ON "User"("passportSeries");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Client_passportSeries_key" ON "Client"("passportSeries");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RequestTour_requestId_key" ON "RequestTour"("requestId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RequestTicket_requestId_key" ON "RequestTicket"("requestId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RequestVisa_requestId_key" ON "RequestVisa"("requestId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Invoice_requestId_key" ON "Invoice"("requestId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Currency_date_key" ON "Currency"("date");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "Branch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "Branch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RequestTour" ADD CONSTRAINT "RequestTour_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "Request"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RequestTicket" ADD CONSTRAINT "RequestTicket_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "Request"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RequestVisa" ADD CONSTRAINT "RequestVisa_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "Request"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "Request"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "Branch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "Branch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
