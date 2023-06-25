454db4f2d3246b3fc64292081a4151fd21caa0fc
Ismet1999 <kadyrovismet1999@gmail.com>
2023-06-25 17:38:29 +0500

    feat: add order number to order

--- src/order/order.entity.ts
+++ src/order/order.entity.ts
@@ -3,6 +3,7 @@ import { Order as OrderModel } from '@prisma/client';
 export class Order implements OrderModel {
   id: string;
   type: string;
+  orderNumber: bigint;
   createdAt: Date;
   clientId: string;
   userId: string;
